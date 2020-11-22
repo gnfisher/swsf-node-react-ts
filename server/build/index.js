a80dea647eb5f7ddfe46777f9ff037c8df99bdb0
Greg Fisher <gnfisher@thoughtbot.com>
2020-11-22 11:50:57 -0500

    Setup barebones react client
    So, you have to delete the `.tsconfig` in the `client/` dir every time you
    restart. Apparently [this is
    fixed](https://github.com/facebook/create-react-app/issues/9868) but it hasn't
    been released onto npm yet, when it is released, I should upgrade!

--- server/build/index.js
+++ server/build/index.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-var PORT = 3000;
+var PORT = 3001;
 app.get('/', function (_, res) {
     res.send('Hello, world...');
 });
