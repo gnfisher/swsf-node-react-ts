375cb1a2c2ab47295652a618dca397e3da3e9379
Greg Fisher <gnfisher@thoughtbot.com>
2020-12-19 20:26:35 -0500

    Hot reloading for React!

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,12 +4,20 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+function render() {
+  return ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  )
+}
+
+render()
+
+if (process.env.NODE_ENV === 'development') {
+  ;(module as any).hot?.accept('./App', render)
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
